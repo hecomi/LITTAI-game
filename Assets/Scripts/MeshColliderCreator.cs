3fe5361a401ca8ebec4e4909e0edd40c7a77720b
hecomi <info@hecomi.com>
2015-07-05 15:13:19 +0900

    initial commit

---
+++ Assets/Scripts/MeshColliderCreator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter)), RequireComponent(typeof(MeshCollider))]
+public class MeshColliderCreator : MonoBehaviour
+{
+	private MeshFilter   meshFilter_;
+	private MeshCollider meshCollider_;
+
+	void Start()
+	{
+		meshFilter_   = GetComponent<MeshFilter>();
+		meshCollider_ = GetComponent<MeshCollider>();
+	}
+
+	void Update()
+	{
+		meshCollider_.sharedMesh = meshFilter_.sharedMesh;
+	}
+}
---
+++ Assets/Scripts/PolygonCreator.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+[RequireComponent(typeof(MeshFilter))]
+public class PolygonCreator : MonoBehaviour
+{
+	public float height = 1f;
+
+	private List<Vector3> polygon_ = new List<Vector3>();
+	public List<Vector3> polygon 
+	{
+		get { return polygon_; }
+		set { polygon_ = value; }
+	}
+	private List<int> indices_ = new List<int>();
+	public List<int> indices
+	{
+		get { return indices_; }
+		set { indices_ = value; }
+	}
+	private MeshFilter meshFilter_;
+
+	void Start()
+	{
+		meshFilter_ = GetComponent<MeshFilter>();
+		Test ();
+	}
+
+	void Update()
+	{
+		if (polygon_.Count == 0) return;
+
+		int vertexNum = polygon_.Count();
+		int indexNum  = indices_.Count();
+		var vertices  = polygon_.Concat(polygon_).ToList();
+		var indices   = indices_.Concat(indices_.ToArray().Reverse()).ToList();
+
+		for (int i = 0; i < vertexNum * 2; ++i) {
+			if (i < vertexNum) {
+				vertices[i] += Vector3.up * height * 0.5f;
+			} else {
+				vertices[i] -= Vector3.up * height * 0.5f;
+			}
+		}
+
+		for (int i = indexNum; i < indexNum * 2; ++i) {
+			indices[i] += vertexNum;
+		}
+
+		for (int i = 0; i < vertexNum - 1; ++i) {
+			var i1 = i;
+			var i2 = i + 1;
+			var i3 = i + vertexNum;
+			var i4 = i + vertexNum + 1;
+			indices.Add(i1);
+			indices.Add(i3);
+			indices.Add(i4);
+			indices.Add(i1);
+			indices.Add(i4);
+			indices.Add(i2);
+		}
+		indices.Add(vertexNum - 1);
+		indices.Add(vertexNum * 2 - 1);
+		indices.Add(vertexNum);
+		indices.Add(vertexNum - 1);
+		indices.Add(vertexNum);
+		indices.Add(0);
+
+		var mesh = new Mesh();
+		mesh.name = "Custom Mesh";
+
+		mesh.vertices = vertices.ToArray();
+		mesh.triangles = indices.ToArray();
+
+		mesh.RecalculateNormals();
+		for (int i = 0; i < indexNum / 3; ++i) {
+			mesh.normals[i] = Vector3.up;
+		}
+		for (int i = indexNum / 3; i < indexNum * 2 / 3; ++i) {
+			mesh.normals[i] = Vector3.down;
+		}
+
+		meshFilter_.sharedMesh = mesh;
+	}
+
+	[ContextMenu("Test")]
+	void Test()
+	{
+		polygon_.Clear();
+		polygon_.Add(new Vector3(-0.18958333333333333f, 0f, 0.039583333333333304f) * 10);
+		polygon_.Add(new Vector3(-0.21041666666666664f, 0f, 0.1645833333333333f) * 10);
+		polygon_.Add(new Vector3(-0.18541666666666667f, 0f, 0.1791666666666667f) * 10);
+		polygon_.Add(new Vector3(-0.18958333333333333f, 0f, 0.22083333333333333f) * 10);
+		polygon_.Add(new Vector3(-0.10625000000000001f, 0f, 0.23541666666666672f) * 10);
+		polygon_.Add(new Vector3(-0.09166666666666667f, 0f, 0.15208333333333335f) * 10);
+		polygon_.Add(new Vector3(-0.17708333333333331f, 0f, 0.1333333333333333f) * 10);
+		polygon_.Add(new Vector3(-0.16458333333333336f, 0f, 0.039583333333333304f) * 10);
+		indices_.Add(7);
+		indices_.Add(0);
+		indices_.Add(1);
+		indices_.Add(6);
+		indices_.Add(7);
+		indices_.Add(1);
+		indices_.Add(5);
+		indices_.Add(6);
+		indices_.Add(1);
+		indices_.Add(5);
+		indices_.Add(1);
+		indices_.Add(2);
+		indices_.Add(5);
+		indices_.Add(2);
+		indices_.Add(3);
+		indices_.Add(5);
+		indices_.Add(3);
+		indices_.Add(4);
+	}
+}
