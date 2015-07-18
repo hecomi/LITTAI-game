9e31bc3476cd3f3162045dbeebe51a084ac9c8b7
hecomi <info@hecomi.com>
2015-07-18 17:24:37 +0900

    player ui follows player

---
+++ Assets/PlayerUiMotion.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerUiMotion : MonoBehaviour
+{
+	public Transform parentPlayer;
+	public float distance = 0.1f;
+	public float damping = 0.1f;
+
+	private LineRenderer line;
+
+	void Start()
+	{
+		line = GetComponent<LineRenderer>();
+		line.SetVertexCount(2);
+	}
+
+	void LateUpdate()
+	{
+		var dir = (parentPlayer.position - transform.position).normalized;
+		var targetPos = parentPlayer.position - dir * distance;
+		transform.position += (targetPos - transform.position) * damping;
+
+		var offset = Vector3.down * 1f;
+		line.SetPosition(1, transform.position + offset);
+		line.SetPosition(0, parentPlayer.position + offset);
+	}
+}
